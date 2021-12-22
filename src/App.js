1683dfbcf90713130b4083e0665bcceb79407548
Ronnie-Leon76 <ronleon76@gmail.com>
2021-12-22 14:20:51 +0300

    React Mini-Projects

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense'
+ 
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Denim Jacket",
+      amount: 1500,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Calculator",
+      amount: 2300,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Coffee Maker",
+      amount: 2000,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
